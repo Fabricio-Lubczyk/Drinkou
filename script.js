7b8df2565b0d631a4d9101cf5e396dc866add57f
Fabricio-Lubczyk <fabriciosamuel737@gmail.com>
2026-03-28 11:49:36 -0300

    remoção javascript, pode nao

--- script.js
+++
@@ -1,9 +0,0 @@
-// animação seguindo o mouse
-const follower = document.getElementById('cursor-follower');
-
-document.addEventListener('mousemove', (e) => {
-    const { clientX, clientY } = e;
-    follower.style.left = `${clientX}px`;
-    follower.style.top = `${clientY}px`;
-});
-
